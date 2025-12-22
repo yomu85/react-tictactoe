cbd289481c731e1f27b543598787267739912939
jacob <yomu85@gmail.com>
2025-12-22 14:23:19 +0900

    day3 완료

--- src/day03/index3.jsx
+++ src/day03/index3.jsx
@@ -1,13 +1,19 @@
-import Profile from "./Profile.jsx"
-
+// aria- 속성*: aria-label, aria-hidden (대시 유지)
+// data- 속성*: data-id, data-value (대시 유지)
 // 챌린지1
-export default function Gallery() {
+export default function Bio() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <>
+      <div class="intro">
+        <h1>Welcome to my website!</h1>
+      </div>
+      <p class="summary">
+        You can find my thoughts here.
+        <br /><br />
+        <b>And <i>pictures</i></b> of scientists!
+      </p>
+    </>
   );
 }
+
+
